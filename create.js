ec08d32c46eef93d55d7fbd2ca09033f7aaaeb44
WillPilgrim <saturner65@gmail.com>
2018-06-18 19:14:11 +0800

    More new table stuff

--- create.js
+++ create.js
@@ -5,7 +5,7 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.tableName1,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       accountId: uuid.v1(),
